3ef6c2cf3aaddbbf6bf98d4a23866976519e2070
Ryan-D-Clark <Ryan@clarkonline.me.uk>
2023-11-16 15:41:02 +0000

    RC:Working Tic Tac Toe, redesign needed

--- external.js
+++ external.js
@@ -1,177 +1,294 @@
-// Factory
-
-const personFactory = (name, age) => {
-    const sayHello = () => console.log('hello!');
-    return { name, age, sayHello };
-};
-  
-const jeff = personFactory('jeff', 27);
-
-console.log(jeff.name); // 'jeff'
-
-jeff.sayHello(); // calls the function and logs 'hello!'
-
-let tiles = document.getElementsByClassName("tile")
-let playerSymbol = "x"
-let computerSymbol = "o"
-let winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
-
-let AIController = (() => {
-    let playableTiles = new Array(9)
-    let playableList
+let board = document.getElementById("board");
+let tiles = document.getElementsByClassName("tile");
+let dialogContainer = document.getElementById("dialogContainer");
+let dialog = document.getElementById("dialog");
+dialogContainer.addEventListener("click", () => {
+    boardController.clearBoard()
+    dialogContainer.close()
+});
+let closeFormButton = document.getElementById("closeForm");
+let playerName = document.getElementById("playerNameInput");
+let gameForm = document.getElementById("gameForm");
+let footer = document.getElementById("footer");
+let radioButtons = document.querySelectorAll('input[name="difficulty"]');
+let selectedDifficulty;
 
 
-    let checkPlayableTiles = () => {
-        playableList = []
-        for(let pos = 0; pos < gameController.board.length; pos++){
-            if(gameController.board[pos] == "x" || gameController.board[pos] == "o"){
-                playableTiles[pos] = "NA"
+closeFormButton.addEventListener("click", () =>{
+    event.preventDefault();
+    if(playerName.value.length <= 0){
+        playerName.value = "Player";
+    }
+
+    for(let radio of radioButtons){
+        if(radio.checked){
+            selectedDifficulty = radio.value;
+        }
+    }
+    gameController.playerName = playerName.value;
+    gameController.setPlayerName()
+    gameForm.style.display = "none";
+    footer.style.display = "none";
+})
+
+let computerScoreDisplay = document.getElementById("computerScore");
+let playerScoreDisplay = document.getElementById("playerScore");
+
+let boardController = (() =>{
+    let board = new Array(9)
+
+    let makeMove = (index, sign) =>{
+        tiles[index].innerText = sign;
+        board[index] = sign;
+    }
+
+
+    let clearBoard = () =>{
+        for(let i = 0; i < tiles.length; i++){
+            tiles[i].innerHTML = "";
+        }
+        for(let i = 0; i < board.length; i++){
+            board[i] = "";
+        }
+    }
+
+    let getEmptyCells = (currentBoard) =>{
+        let playableCells = [];
+        for(let i = 0; i < currentBoard.length; i++){
+            if(currentBoard[i] == gameController.AISign || currentBoard[i] == gameController.playerSign){
             }
             else{
-                playableList.push(pos)
+                playableCells.push(i);
             }
         }
-        if(playableList.length == 0){
-            console.log("No available spots")
+        return playableCells;
+    }
+
+    for(let i = 0; i < tiles.length; i++){
+        tiles[i].addEventListener("click", () =>{
+            if(tiles[i].innerText == gameController.AISign || tiles[i].innerText == gameController.playerSign){
+                return;
+            }
+            makeMove(i, gameController.playerSign)
+            if(gameController.gameTracker()){
+                return;
+            }
+            if(selectedDifficulty === "impossible" || selectedDifficulty == null){
+                makeMove(AIController.takeIntelligentTurn(board, gameController.AISign),gameController.AISign)
+            }else if(selectedDifficulty === "hard"){
+                makeMove(AIController.takeHardTurn(board, gameController.AISign), gameController.AISign)
+            }else if(selectedDifficulty === "normal"){
+                makeMove(AIController.takeNormalTurn(board,gameController.AISign), gameController.AISign)
+            }
+            
+            gameController.gameTracker()
+        })}
+    return{
+        board, makeMove, clearBoard, getEmptyCells
+    };
+})();
+
+let gameController = (() =>{
+    let playerName = null
+    let playerSign = "X";
+    let AISign = "O" ;
+    let playerScore = 0;
+    let AIScore = 0;
+
+
+    let checkIfWinnerFound = (currentBoard, currentSign) => {
+        if (
+            (currentBoard[0] === currentSign && currentBoard[1] === currentSign && currentBoard[2] === currentSign) ||
+            (currentBoard[3] === currentSign && currentBoard[4] === currentSign && currentBoard[5] === currentSign) ||
+            (currentBoard[6] === currentSign && currentBoard[7] === currentSign && currentBoard[8] === currentSign) ||
+            (currentBoard[0] === currentSign && currentBoard[3] === currentSign && currentBoard[6] === currentSign) ||
+            (currentBoard[1] === currentSign && currentBoard[4] === currentSign && currentBoard[7] === currentSign) ||
+            (currentBoard[2] === currentSign && currentBoard[5] === currentSign && currentBoard[8] === currentSign) ||
+            (currentBoard[0] === currentSign && currentBoard[4] === currentSign && currentBoard[8] === currentSign) ||
+            (currentBoard[2] === currentSign && currentBoard[4] === currentSign && currentBoard[6] === currentSign)
+        ){
+            return true;
         }
         else{
-            makeMove()
+            return false;
+        }
+
+    }
+
+    let setPlayerName = () =>{
+        let playerNameDisplay = document.getElementById("playerName")
+        playerNameDisplay.innerHTML = gameController.playerName
+    }
+
+    let updateScores = () => {
+        computerScore.innerText = AIScore;
+        playerScoreDisplay.innerText = playerScore;
+    }
+
+    let gameTracker = () => {
+
+        if(checkIfWinnerFound(boardController.board, playerSign)){
+            playerScore += 1
+
+            if(playerScore == 3){
+                console.log("test 3")
+                dialog.innerHTML = `Game Winner <br> ${gameController.playerName}`
+                dialogContainer.showModal()
+                AIScore = 0
+                playerScore = 0
+                updateScores()
+            }else{
+                dialog.innerHTML = `Round Winner <br> ${playerSign}`
+                dialogContainer.showModal()
+                updateScores()
+            }
+
+
+            return true
+        }else if(checkIfWinnerFound(boardController.board, AISign)){
+            AIScore += 1
+            if(AIScore == 3){
+                dialog.innerHTML = `Game Winner <br> Computer`
+                dialogContainer.showModal()
+                AIScore = 0
+                playerScore = 0
+                updateScores()
+            }
+            else{
+                dialog.innerHTML = `Round Winner <br> ${AISign}`
+                dialogContainer.showModal()
+                updateScores()
+            }   
+
+            return true
+        }
+
+        else if(boardController.getEmptyCells(boardController.board).length == 0){
+            dialog.innerHTML = `Round Draw`
+            dialogContainer.showModal()
+            return true
         }
     }
-    
-    let makeMove = () => {
-        console.log("AI playable spots:", playableList)
-        let randomPos = playableList[Math.floor(Math.random() * playableList.length)]
-        console.log("Picked index:", randomPos)
-        gameController.board[randomPos] = computerSymbol
-        tiles[randomPos].innerHTML = computerSymbol
-        gameController.checkRound(computerSymbol)
-    }
+
+
     return{
-        checkPlayableTiles,
-        makeMove
-    }
+        playerSign, playerScore, AISign, AIScore, playerName, setPlayerName, checkIfWinnerFound, updateScores, gameTracker
+    };
+})();
+
+let AIController = (() =>{
+
+    let minimax = (currentBoard, currentSign) => {
+
+        let availableCells = boardController.getEmptyCells(currentBoard)
+
+        if(gameController.checkIfWinnerFound(currentBoard, gameController.playerSign)){
+            return {score: -1};
+        }else if(gameController.checkIfWinnerFound(currentBoard,gameController.AISign)){
+            return {score: 1};
+        } else if(availableCells.length === 0){
+            return {score: 0};
+        }
+
+        let minimaxOutcomes = [];
+
+        for(let i = 0; i < availableCells.length; i++){
+            let currentLoop = {};
+            currentLoop.index = availableCells[i];
+            currentBoard[availableCells[i]] = currentSign;
+            if(currentSign === gameController.AISign){
+                let result = minimax(currentBoard, gameController.playerSign)
+                currentLoop.score = result.score;
+            } else{
+                let result = minimax(currentBoard, gameController.AISign)
+                currentLoop.score = result.score
+            }
+            currentBoard[availableCells[i]] = currentLoop.index
+            minimaxOutcomes.push(currentLoop)
+        }
+        let bestLoopScore = null;
+
+        if(currentSign === gameController.AISign){
+            let bestScore = -Infinity;
+            for(let i = 0; i < minimaxOutcomes.length; i++){
+                if(minimaxOutcomes[i].score > bestScore){
+                    bestScore = minimaxOutcomes[i].score;
+                    bestLoopScore = i
+                }
+            }
+        }
+        else{
+            let bestScore = Infinity;
+            for(let i = 0; i < minimaxOutcomes.length; i++){
+                if(minimaxOutcomes[i].score < bestScore){
+                    bestScore = minimaxOutcomes[i].score;
+                    bestLoopScore = i
+                }
+            }
+        }
+        return minimaxOutcomes[bestLoopScore]
+        }
+
+        let takeIntelligentTurn = (currentBoard, currentSign) =>{
+            let bestPlay = minimax(currentBoard, currentSign)
+            return bestPlay.index 
+        }
+
+        let takeNormalTurn = (currentBoard, currentSign) =>{
+            let intelligentTile = minimax(currentBoard, currentSign)
+            let availableCells = boardController.getEmptyCells(currentBoard)
+            let randomTile = availableCells[Math.floor(Math.random() * availableCells.length)];
+            if(Math.floor(Math.random() * 11) %2){
+                return intelligentTile.index
+            } 
+            else{
+                return randomTile
+            }
+        }
+
+        let takeHardTurn = (currentBoard, currentSign) => {
+            let winningCombinations = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
+              ];
+              for(let combination of winningCombinations){
+                let counter = 0
+                for(let i = 0; i < combination.length; i++){
+                    if(boardController.board[combination[i]] == gameController.playerSign){
+                        counter += 1
+                        if(counter == 2){
+                            for(let x = 0; x < combination.length; x++){
+                                if(boardController.board[combination[x]] != gameController.playerSign && boardController.board[combination[x]] != gameController.AISign){
+                                    return combination[x]
+                                }
+                            }
+                        }
+                    }
+                }
+              }
+            
+            let intelligentTile = minimax(currentBoard, currentSign)
+            let availableCells = boardController.getEmptyCells(currentBoard)
+            let randomTile = availableCells[Math.floor(Math.random() * availableCells.length)];
+            if(Math.floor(Math.random() * 11) %2){
+                return intelligentTile.index
+            } 
+            else{
+                return randomTile
+            }
+        }
+
+    return{
+        minimax, takeIntelligentTurn, takeNormalTurn, takeHardTurn
+    };
 
 
 })();
 
-let gameController = (() => {
-    let board = new Array(9);
-    let computerScore = 0;
-    let playerScore = 0;
-    let currentlyPlaying = true
-    let logBoard = () => console.log(board);
-
-    let checkRound = (symbol) => {
-        console.log("Testing", board.length)
-        for(combination in winningCombinations){
-            let counter = 0
-            for(pos in winningCombinations[combination]){
-                if(gameController.board[winningCombinations[combination][pos]] == symbol){
-                    counter += 1
-                }else{
-                    break
-                }}
-            if(counter == 3){
-                displayController.showRoundWinner(symbol)
-                currentlyPlaying = false
-                roundFinisher(symbol)
-            }}}
-            
-    for(let i = 0; i < tiles.length; i++){
-        tiles[i].addEventListener("click", function(){
-            if(gameController.board[i] == playerSymbol || gameController.board[i] == computerSymbol){
-                console.log("Already picked")
-                displayController.showPlayerError()}
-            else{
-                gameController.board[i] = playerSymbol
-                tiles[i].innerHTML = playerSymbol
-                console.log(gameController.board)
-                checkRound(playerSymbol)
-                if(currentlyPlaying){
-                    AIController.checkPlayableTiles()
-                }
-                else{
-                    currentlyPlaying = true
-                }
-            }})}
-
-    let roundFinisher = (symbol) => {
-        if(symbol == playerSymbol){
-            playerScore += 1
-        }
-        else{
-            computerScore +=1
-        }
-        gameController.board = new Array(9)
-        for(let i = 0; i < tiles.length; i++){
-            tiles[i].innerHTML = ""
-        }
-        console.log(`${symbol} won that round!`)
-        console.log("Player Score:", playerScore, "Computer Score:", computerScore)
-    }
-    return {
-        board,
-        logBoard,
-        checkRound,
-        roundFinisher,
-        currentlyPlaying,
-        computerScore,
-        playerScore
-    }})();
-
-let displayController = (() => {
-    let alertContainer = document.getElementById("alert-container")
-    let playerError = document.getElementById("player-error")
-    let roundWinner = document.getElementById("round-winner")
-    let gameWinner = document.getElementById("game-winner")
-    let roundDraw = document.getElementById("round-draw")
-
-
-    alertContainer.addEventListener("click", function(){
-        alertContainer.style.display = "none"
-        playerError.style.display = "none"
-        roundWinner.style.display = "none"
-        gameWinner.style.display = "none"
-
-    })
-
-    let showPlayerError = () => {
-        alertContainer.style.display = "block"
-        playerError.style.display = "block"
-    }
-
-    let showRoundWinner = (symbol) => {
-        alertContainer.style.display = "block"
-        roundWinner.innerHTML = `Round winner<br>${symbol}<br><br>Click to continue...`
-        roundWinner.style.display = "block"
-    }
-
-    let showRoundDraw = () => {
-        alertContainer.style.display = "block"
-        roundDraw.style.display = "block"
-    }
-
-    let showGameWinner = () => {
-        alertContainer.style.display = "block"
-        gameWinner.style.display = "block"
-
-    }
-    return {
-        alertContainer,
-        playerError,
-        showPlayerError,
-        showRoundWinner,
-        showGameWinner,
-        showRoundDraw
-    }})();
 
